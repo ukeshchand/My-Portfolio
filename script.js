48e8b1e51653ea98015282fd3579b4a7660b56bf
UkeshChand <ukeshc4@gmail.com>
2023-08-10 14:19:25 +0545

    Made responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function() {
-    alert("This website is currently under construction. I apologize for any inconvenience. Please check back soon");
+    alert("This website is still not responsive so, it might not support in your device. It is still under development. I apologize for any inconvenience. Please check back soon. Thankyouu! Ukesh Chand:)");
   });
   
